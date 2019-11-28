7a17cf615d77900ba57b6f379e2fb6bb7be219e5
BELONG1997520 <44727232+BELONG1997520@users.noreply.github.com>
2019-11-28 17:54:38 +0800

    Initial commit

---
+++ day01_day05/src/main/Java/day01/T_01HelloWorld.java
@@ -0,0 +1,7 @@
+package day01;
+
+public class T_01HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ day01_day05/src/main/Java/day01/T_02Homework04.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class T_02Homework04 {
+    public static void main(String[] args) {
+        System.out.println("姓名：习大大");
+        System.out.println("性别：男");
+        System.out.println("地址：北京");
+    }
+}
